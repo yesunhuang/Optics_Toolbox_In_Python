c00be260bb4ffa405005d31c39ba43be4316a794
yesunhuang <yesunhuang@mail.ustc.edu.cn>
2022-05-20 15:11:52 -0400

    Update

---
+++ __init__.py
@@ -0,0 +1,9 @@
+'''
+Name: __init__
+Desriptption: the init py for the package
+Email: yesunhuang@mail.ustc.edu.cn
+OpenSource: https://github.com/yesunhuang
+Msg: For the course "Fundamental of modern optics"
+Author: YesunHuang
+Date: 2022-04-08 20:25:45
+'''
